9884b29e2aca0f2274c521fc8aa600fa14672532
Jesús Gómez <zosemu@gmail.com>
2019-10-11 13:37:49 -0500

    Create get_snaps.py

---
+++ get_snaps.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+k = os.popen('whoami').read().strip()
+
+path_kde = '/usr/share/applications/'
+path_snap = '/var/lib/snapd/desktop/applications/'
+
+if k == 'root':    
+    z = os.popen('ls {}*.desktop'.format(path_kde)).read().replace(path_kde,'').split('\n')
+    o = os.popen('ls {}*.desktop'.format(path_snap)).read().replace(path_snap,'').split('\n')
+    o.pop(-1)
+    z.pop(-1)
+    for x in o:
+        s = 0
+        if x not in z:
+            e = "cp {}{} {}{}".format(path_snap,x,path_kde,x)
+            os.popen(e)
+            s += 1
+    print "[+] Apps founded: {}\n[+] Apps copied: {}".format(len(o),e)
+else:
+    print "Run the script as root!!"
